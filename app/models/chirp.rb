0ca887b8ac48cde2f794da2ce6d0f465c34e645a
Mitch Smith <mitchpsmith1998@gmail.com>
2025-04-10 23:54:39 +1000

    Lint using Rubocop

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -10,7 +10,7 @@ class Chirp < ApplicationRecord
   end
 
   def interested_users
-    [author] | author.followers | mentions
+    [ author ] | author.followers | mentions
   end
 
   def self.firehose
@@ -21,11 +21,11 @@ class Chirp < ApplicationRecord
     broadcast_prepend_later_to "firehose", target: "firehose"
 
     # Update the author's page
-    broadcast_prepend_later_to ["chirps", author.id], target: "chirps"
+    broadcast_prepend_later_to [ "chirps", author.id ], target: "chirps"
 
     # Update the timelines of all users who are interested in this chirp
     interested_users.each do |follower|
-      broadcast_prepend_later_to ["timeline", follower.id], target: "timeline"
+      broadcast_prepend_later_to [ "timeline", follower.id ], target: "timeline"
     end
   end
 
